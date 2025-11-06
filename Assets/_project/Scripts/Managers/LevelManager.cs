8d216d1909a94eed03f5c9adbd2e53cac51ff670
gokcegun <gokce@locomotive.games>
2025-11-06 20:22:27 +0300

    Initial commit

---
+++ Assets/_project/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int levelNo;
+
+    public List<Level> levelPrefabs;
+
+    private Level _curLevel;
+
+    public void RestartLevelManager()
+    {
+        DestroyPreviousLevel();
+        CreateNewLevel();
+    }
+
+    private void DestroyPreviousLevel()
+    {
+        if (_curLevel != null)
+        {
+            Destroy(_curLevel.gameObject);
+        }
+    }
+
+    private void CreateNewLevel()
+    {
+        levelNo = Math.Clamp(levelNo, 1, levelPrefabs.Count);
+        _curLevel = Instantiate(levelPrefabs[levelNo-1]);
+    }    
+
+    public void LoadNextLevel()
+    {
+
+    }
+
+    public void LoadPreviousLevel()
+    {
+
+    }
+}
